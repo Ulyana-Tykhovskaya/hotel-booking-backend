c4eb1c18d587531fd039142852cb4208dd21af54
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2026-06-20 12:17:15 +0300

    Fix debag

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  username: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  port: Number(process.env.DB_PORT || process.env.MYSQLPORT),
   dialect: "mysql",
   logging: false,
 });
