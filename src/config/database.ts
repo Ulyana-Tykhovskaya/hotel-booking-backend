5e6c8425f8c93c4fab58dfd78947e1ff0619da70
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2026-06-20 12:28:09 +0300

    debag

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,14 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
-  username: process.env.DB_USER || process.env.MYSQLUSER,
-  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-  host: process.env.DB_HOST || process.env.MYSQLHOST,
-  port: Number(process.env.DB_PORT || process.env.MYSQLPORT),
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    dialect: "mysql",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+);
 
 export default sequelize;
